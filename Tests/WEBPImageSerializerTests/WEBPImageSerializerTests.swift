30dcd02a419960a38a048c5fef2edaff72bc18e7
Jeffrey Macko <github.jm@macko.fr>
2024-09-11 14:10:07 +0200

    feat: cleaning

--- Tests/WEBPImageSerializerTests/WEBPImageSerializerTests.swift
+++ Tests/WEBPImageSerializerTests/WEBPImageSerializerTests.swift
@@ -1,5 +1,5 @@
 import Testing
-@testable import swift_snapshot_testing_plugin_webp
+@testable import WEBPImageSerializer
 
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
